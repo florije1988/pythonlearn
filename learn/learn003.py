2001d80da2386a9e09db2bd9e2277b57252fd889
florije1988 <florije1988@gmail.com>
2014-06-03 13:40:02 +0800

    add the 'learn003.py' file to the project

--- learn/learn003.py
+++ learn/learn003.py
@@ -3,3 +3,4 @@ __author__ = 'florije'
 
 # there nothing just a test to add something odd to this file
 # also something to test the push code to remote
+# you will see it when you push this to the project
