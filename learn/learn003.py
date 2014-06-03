b630983409ce8e46184ec8e26ebceadde7a954c6
florije1988 <florije1988@gmail.com>
2014-06-03 15:17:33 +0800

    some 'lambda' and some 'map' and some 'reduce' and some 'zip'

--- learn/learn003.py
+++ learn/learn003.py
@@ -1,6 +1,32 @@
 # -*- coding: utf-8 -*-
 __author__ = 'florije'
 
-# there nothing just a test to add something odd to this file
-# also something to test the push code to remote
-# you will see it when you push this to the project
+# this is the program to test the func map and reduce
+
+# map( func, seq1[, seq2...] )
+
+if __name__ == '__main__':
+    print range(6)
+    print [x % 3 for x in range(6)]
+    print map(lambda x: x % 3, range(6))
+
+    a = [1, 2, 3]
+    b = [4, 5, 6]
+    zipped = zip(a, b)
+    print zipped
+    print zip(*zipped)
+
+    print map(lambda x, y: x * y, [1, 2, 3], [4, 5, 6])
+    print map(lambda x, y, z: (x * y + z, x + y - z), [1, 2, 3], [4, 5, 6], [1, 2, 3])  # [(4, 5), (10, 7), (18, 9)]
+
+    print map(None, [1, 2, 3], [4, 5, 6])  # [(1, 4), (2, 5), (3, 6)]
+
+    print zip([1, 2, 3], [4, 5, 6])  # [(1, 4), (2, 5), (3, 6)]
+
+    # reduce( func, seq[, init] )
+    n = 5
+    print reduce(lambda x, y: x * y, range(1, n + 1))  # 120
+    m = 2
+    n = 5
+    print reduce(lambda x, y: x * y, range(1, n + 1), m)
+
