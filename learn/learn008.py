e1fd8924b41ea272b5246962a2d5ea8ed31a3053
florije1988 <florije1988@gmail.com>
2014-06-04 13:28:14 +0800

    modify this method, modify this method, modify this method, modify this method,

--- learn/learn008.py
+++ learn/learn008.py
@@ -23,14 +23,38 @@ class Person(object):
 
 
 class Student(Person):
+    '''
+    This is the Student class
+    '''
     def __init__(self, name, age):
+        '''
+        this is the init func in class Student
+        :param name:
+        :param age:
+        :return:
+        '''
         super(Student, self).__init__(name)
         self.age = age
 
     def show(self):
+        '''
+        This is the show func in class Student
+        :return:nothing
+        '''
         print 'self.name: %s, self.age: %s' % (self.name, self.age)
+
+    def operation(self, a, b):
+        '''
+
+        :param a:
+        :param b:
+        :return:
+        '''
+        return a + b
 
 
 if __name__ == '__main__':
     student = Student('florije', 15)
+    print student.__doc__
     student.show()
+    student.operation(1, 2)
